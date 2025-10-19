334d0fff591c2b8df3325c57746df2f96b102ec9
Silver Ling <silver.ling@outlook.com>
2025-10-19 18:01:58 +0800

    refactor(preview): use uniform sampler and remove unused fps parameter

--- src/tools/preview.py
+++ src/tools/preview.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 import tyro
 
+from ome.io.sample.uniform import UniformSampler
 from ome.repr.frame import events_to_grayscale_count_cuda
 from ome.utils.timer import Timer
 
@@ -10,7 +11,6 @@ def main(
     file: Path,
     reader: str,
     ms_per_frame: int = 33,
-    fps: float | None = None,
     display: bool = False,
 ):
     if reader == "tumvie":
@@ -41,19 +41,21 @@ def main(
 
         import tools.window
 
-        ms_per_frame_float = ms_per_frame if fps is None else 1000 / fps
-        window = tools.window.Window(file.name, ms_per_frame_float)
+        window = tools.window.Window(file.name, ms_per_frame)
 
-    ticks = list(range(0, event_reader.max_ms, ms_per_frame))
-
+    sampler = UniformSampler(event_reader, sample_rate=1, duration_ms=ms_per_frame)
     timer = Timer()
-    for idx, tick in enumerate(ticks, start=1):
+    for idx, tick in enumerate(sampler.get_all_timestamps(anchor="start")):
         timer.tick()
-        x, y, p, t = event_reader.duration(tick, ms_per_frame)
+        x, y, p, t = sampler[idx]
         frame = events_to_grayscale_count_cuda(x, y, width=event_reader.width, height=event_reader.height)
         elapsed = timer.elapsed()
 
-        print(f"[{idx}/{len(ticks)}] | Number of events: {len(x)} | Elapsed time: {elapsed * 1e3:.2f} ms")
+        print(
+            f"[{idx + 1}/{len(sampler)}] | Number of events: {len(x)}"
+            f" | Processing time: {elapsed * 1e3:.2f} ms"
+            f" | Event time: {tick} - {tick + ms_per_frame} ms"
+        )
 
         if display and window.show(frame, elapsed * 1e3):
             break
